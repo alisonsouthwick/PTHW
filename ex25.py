e9aebf1a2c1d7432a57f45f7a1efe0845160a628
alisonsouthwick <milquetoast@gmail.com>
2015-08-07 17:40:17 -0400

    Initial

---
+++ ex25.py
@@ -0,0 +1,47 @@
+def break_words(stuff):
+	"""This function will break up words for us."""
+	words = stuff.split(' ')
+	return words
+
+def sort_words(words):
+	"""Sorts the words."""
+	return sorted(words)
+
+def print_first_word(words):
+	"""Prints the first word after popping it off."""
+	word = words.pop(0)
+	print word
+
+def print_last_word(words):
+	"""Prints the last word after popping it off."""
+	word = words.pop(-1)
+	print word
+
+def sort_sentence(sentence):
+	"""Takes in a full sentence and returns the sorted words."""
+	words = break_words(sentence)
+	return sort_words(words)
+
+def print_first_and_last(sentence):
+	""""Prints the first and last words of a sentence."""
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""Sorts the words then prints the first and last one."""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+#this is me playing at running the functions. It worked!
+
+my_sentence = sort_sentence("This is my sentence")
+print my_sentence
+
+new_sentence = raw_input("Tell me a story =>")
+
+print sort_sentence(new_sentence)
+print print_first_and_last(new_sentence)
+print print_first_and_last_sorted(new_sentence)
+
